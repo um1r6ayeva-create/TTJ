89073a2d50cda2cd927c1a6380ae548a98b77607
um1r6ayeva-create <um1r6ayeva@gmail.com>
2026-03-15 16:08:31 +0500

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,26 +1,11 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing import List, Optional
+from typing import List
 
 
 class Settings(BaseSettings):
-    # Database variables
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: str = os.getenv("DB_PORT")
-    DB_NAME: str = os.getenv("DB_NAME")
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
 
-    DATABASE_URL: Optional[str] = None
-
-    @property
-    def get_database_url(self) -> str:
-        if self.DATABASE_URL:
-            if self.DATABASE_URL.startswith("postgres://"):
-                return self.DATABASE_URL.replace("postgres://", "postgresql://", 1)
-            return self.DATABASE_URL
-
-        return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
     # API
     DEEPSEEK_API_KEY: str = ""
