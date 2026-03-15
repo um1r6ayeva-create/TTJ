85fa8b231d1316a898f78fc996242c60efe4fe99
um1r6ayeva-create <um1r6ayeva@gmail.com>
2026-03-15 16:09:50 +0500

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,14 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from app.core.config import settings  # или свой путь до настроек
 from sqlalchemy.orm import declarative_base
+
+from app.core.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
-SQLALCHEMY_DATABASE_URL = settings.get_database_url  # URL строки к PostgreSQL
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db: Session = SessionLocal()
